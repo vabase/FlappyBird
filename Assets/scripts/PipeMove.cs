5e2491833dfb40068163d0642e698ea558e8e016
unknown <MylanAngel123@gmail.com>
2022-02-08 16:43:36 +0200

    Create and adjust pipes, create PipeSpawner and corresponding scripts.

---
+++ Assets/scripts/PipeMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMove : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/scripts/PipeSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawn : MonoBehaviour
+{
+
+    [SerializeField] float timeToSpawn = 1f;
+    [SerializeField] float borderTop = 0.8f;
+    [SerializeField] float borderBottom = -0.2f;
+    [SerializeField] GameObject pipes;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPipes());
+    }
+
+    IEnumerator SpawnPipes()
+    {
+        while (true)
+        {
+            GameObject newpipe = Instantiate(pipes);
+            Vector3 randomFactor = new Vector3(0, Random.Range(borderBottom, borderTop), 0);
+            newpipe.transform.position = transform.position + randomFactor;
+            Destroy(newpipe, 5f);
+            yield return new WaitForSeconds(timeToSpawn);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
